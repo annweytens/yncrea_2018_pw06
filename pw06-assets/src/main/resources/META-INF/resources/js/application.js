b7f91a6703aa60fd388883d26cecdbb0fc0bd025
Amaury Willemant <awillemant@gmail.com>
2018-02-16 10:56:14 +0100

    Update application.js

--- pw06-assets/src/main/resources/META-INF/resources/js/application.js
+++ pw06-assets/src/main/resources/META-INF/resources/js/application.js
@@ -1,6 +1,6 @@
 var angularApp = angular.module('tp06', ['ui.calendar', 'ngResource']);
 
-angularApp.factory('EvenementsWS', ['$resource', function ($resource) {
+angularApp.factory('CalEventsWS', ['$resource', function ($resource) {
     return $resource('api/calevents/:id', null,
         {
             'update': {method: 'PUT'}
